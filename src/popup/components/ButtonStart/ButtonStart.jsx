fede8a53e0791402a7b4c7fdd6831ab224cf536b
Victor Igor <victor.0w3@hotmail.com>
2016-10-21 21:46:22 -0400

    :bug: working play button

--- src/popup/components/ButtonStart/ButtonStart.jsx
+++ src/popup/components/ButtonStart/ButtonStart.jsx
@@ -1,13 +1,63 @@
 import React, {Component} from 'react';
+import Alarm from 'shared/Alarms';
 
 class ButtonStart extends Component{
+	constructor(props) {
+		super(props);
+		this.play = this.play.bind(this);
+		this.alarm = new Alarm('remindme', 1);
+		this.state = {
+			titleButton: "Play",
+			play: true
+		}
+	}
+
+	componentDidMount() {
+		chrome.storage.sync.get('playing', (obj) => {
+			if(obj.playing){
+				this.setState({titleButton: "Stop", play: false});
+			}
+		});
+	}
+
+	handleClick(){
+		
+		this.setState({play: !this.state.play});
+
+	}
+
+	play(){
+		this.handleClick();
+		chrome.storage.sync.get('playing', (obj) => {
+			if(obj.playing){
+				chrome.storage.sync.set({playing: false}, () => {
+					
+					console.log('stop');
+					
+					this.alarm.cancel();
+					this.setState({titleButton: "Play"})
+				});
+			}else{
+				chrome.storage.sync.set({playing: true}, () => {
+					
+					console.log('playing');
+					
+					this.alarm.create();
+					this.setState({titleButton: "Stop"})
+				});
+			}
+		});
+	}
 
 	render(){
 		return (
 
 			<section className="start">
-			  <p id="playTitle">click to play</p>
-			  <a className="action-button shadow animate blue">Play</a>
+
+			  <a 	className={"action-button shadow animate " +  (this.state.play ? "blue" : "red") } onClick={this.play}>
+			  		{this.state.titleButton}
+			  </a>
+
 			</section>
 
 		);
