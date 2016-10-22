2b4ce85460f97b81e00687eefdafccd3134d38e7
Victor Igor <victor.0w3@hotmail.com>
2016-10-22 18:22:43 -0400

    progress bar dinamic with point

--- src/popup/components/Points/Points.jsx
+++ src/popup/components/Points/Points.jsx
@@ -1,25 +1,29 @@
 import React, {Component} from 'react';
 import { Line, Circle } from 'rc-progress';
 
+//Max point : 1000
 class Points extends Component {
 
 	constructor(props) {
 		super(props);
 		this.state = {
-			point: "1.800",
+			point: 100,
 			level: 1
 		}
 	}
 
+	componentDidMount() {
+		this.setState({point: 400});
+	}
 	render(){
 		return (
 			<section>
 				<section className="content-progress">
-					 <Line percent="50" strokeWidth="4" strokeColor="#ffd400" />
+					 <Line percent={''+ (this.state.point/100) * 10} strokeWidth="4" strokeColor="#ffd400" />
 				</section>
 				<section className="content-point">
 					<span className="Point-level">Level {this.state.level}</span>
-					<span className="Point-over">1.120 Pts to Level {this.state.level + 1}</span>
+					<span className="Point-over">{1000 - this.state.point} Pts to Level {this.state.level + 1}</span>
 					<span className="Point-point">{this.state.point}</span>
 				</section>
 			</section>
