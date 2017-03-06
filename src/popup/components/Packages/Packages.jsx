ba78487454893c3a013e66bdcecd0ed2876a8704
Victor Igor <victor.0w3@hotmail.com>
2017-03-05 21:52:21 -0300

    trash in packages list

--- src/popup/components/Packages/Packages.jsx
+++ src/popup/components/Packages/Packages.jsx
@@ -5,17 +5,18 @@ import {putStorage, cleanPackages} from 'shared/helpers';
 import AddPackage from '../AddPackage/AddPackage';
 
 class Packages extends Component{
-	constructor(props) {
-		super(props);
-      this.renderPackagesList = this.renderPackagesList.bind(this);
-      this.handlerItemPackage = this.handlerItemPackage.bind(this);
-      this.renderPackageEdit  = this.renderPackageEdit.bind(this);
-      this.state = {
-          addingPackage: false,
-          packages: {},
-          editingPackage: false
-      }
-	}
+    constructor(props) {
+        super(props);
+        this.renderPackagesList = this.renderPackagesList.bind(this);
+        this.handlerItemPackage = this.handlerItemPackage.bind(this);
+        this.renderPackageEdit  = this.renderPackageEdit.bind(this);
+        this.handlerDeletePackage = this.handlerDeletePackage.bind(this);
+        this.state = {
+            addingPackage: false,
+            packages: {},
+            editingPackage: false
+        }
+    }
 
     handlerItemPackage(e){
         console.log(e.currentTarget.getAttribute('title'));
@@ -24,20 +25,30 @@ class Packages extends Component{
         });
     }
 
-  renderPackagesList(){
+    handlerDeletePackage(e){
+        console.log(e.currentTarget);
+    }
 
-      let element = (<div></div>)
+    renderPackagesList(){
 
-      for(let property in this.state.packages){
-          element = [(<li key={property}
-                          title={property}
-                          onClick={this.handlerItemPackage}>
-                          <span>{property}</span>
-                      </li>), ...element ]
-      }
+        let element = (<div></div>)
 
-      return element;
-  }
+        for(let property in this.state.packages){
+            element = [(<li key={property}
+                            title={property}
+                            onClick={this.handlerItemPackage}>
+                            <span className="delete__package" onClick={this.handlerDeletePackage}>
+                                <svg width="15" height="15" viewBox="0 0 64 64">
+                                    <path fill="#fff" d="M24.72 8.777h14.56v3.747H24.72zM7.917 11.56h48.164v4.818H7.918z"/>
+                                    <path fill="none" stroke="#fff" d="M40.212 57.362V27.005M32 57.398V27.04m-8.212 30.394V27.077m-11.06-7.594h38.543v40.254H12.73z"/>
+                                </svg>
+                            </span>
+                            <span className="package__name">{property}</span>
+                        </li>), ...element ]
+        }
+
+        return element;
+    }
 
     renderPackageEdit(name){
         let packageTitleElement, container;
@@ -82,7 +93,7 @@ class Packages extends Component{
                 });
             });
         });
-  }
+    }
 
 	  render(){
 
@@ -99,7 +110,7 @@ class Packages extends Component{
             </section>
         </div>
         );
-	}
+    }
 }
 
 export default Packages;
