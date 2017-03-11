2ee50454f3bcdb458ef6d122a9a708d5aa55caf7
Victor Igor <victor.0w3@hotmail.com>
2017-03-11 11:44:45 -0300

    refactoring button start component to use promise

--- src/popup/components/ButtonStart/ButtonStart.jsx
+++ src/popup/components/ButtonStart/ButtonStart.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Alarm from 'shared/Alarms';
 import PubSub from 'pubsub-js';
-import {putStorage} from 'shared/helpers';
+import {putStorage, getChromeStorage} from 'shared/helpers';
 
 class ButtonStart extends Component{
 	constructor(props) {
@@ -19,14 +19,14 @@ class ButtonStart extends Component{
 
   componentDidMount() {
       /* Check if was already started*/
-    chrome.storage.sync.get('playing', (obj) => {
-        if(obj.playing){
+      getChromeStorage('playing')
+        .then((playing) => {
             this.setState({titleButton: "Stop", play: false});
-        }
-    });
+        })
+        .catch(err => {});
+
     //get value in inputInterval component
     PubSub.subscribe('rangeInterval', (topic, value) => {
-        /* console.log('[ButtonStart]: rangeInterval: ', value);*/
         this.setState({rangeInterval: value})
     });
 
@@ -40,31 +40,27 @@ class ButtonStart extends Component{
     this.setState({play: !this.state.play});
 	}
 
-	play(){
-		this.handleClick();
-		chrome.storage.sync.get('playing', (obj) => {
-			  if(obj.playing){
+  play(){
+    this.handleClick();
+    getChromeStorage('playing')
+        .then( playing => {
             /* If was already started, change the button to play and stop alarm */
             chrome.storage.sync.set({playing: false}, () => {
                 this.alarm.cancel();
-                this.setState({titleButton: "Play"})
+                this.setState({titleButton: "Play"});
             });
-        }else{
-            /* If not was started, to create alarm and change button text to Stop*/
-            chrome.storage.sync.set({playing: true}, () => {
-                this.alarm.cancel();
-                this.alarm = new Alarm('remindme', this.state.rangeInterval); //update Interval
-
-                console.log('[ButtonStart] created with ' + this.state.rangeInterval + ' interval');
-
-                this.alarm.create();
-                this.setState({titleButton: "Stop"})
-                //saving in localStorage, beacause background script  it will take the value
-                putStorage('rangeInterval', this.state.rangeInterval);
-            });
-
-			}
-		});
+    })
+    .catch(() => {
+        /* If not was started, to create alarm and change button text to Stop*/
+        chrome.storage.sync.set({playing: true}, () => {
+            this.alarm.cancel();
+            this.alarm = new Alarm('remindme', this.state.rangeInterval); //update Interval
+            this.alarm.create();
+            this.setState({titleButton: "Stop"})
+            //saving in localStorage, beacause background script  it will take the value
+            putStorage('rangeInterval', this.state.rangeInterval);
+        });
+    });
 	}
 
 	render(){
